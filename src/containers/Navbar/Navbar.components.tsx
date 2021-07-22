72a95f073a23b3775f69543803a6e960a3350f4e
Lars van der Niet <larsvanderniet@gmail.com>
2021-07-22 13:07:45 +0200

    Created a responsive nav-menu

--- src/containers/Navbar/Navbar.components.tsx
+++ src/containers/Navbar/Navbar.components.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import theme from '../../styles/theme'
 import Container from '../../containers/Container'
 
-const { colors } = theme;
+const { colors, containerWidth } = theme;
 
 export const Nav = styled.nav`
     position: fixed;
@@ -25,4 +25,80 @@ export const NavbarLogo = styled.h5`
     margin-bottom: 0;
 `;
 
+
+// // / / /  / / /  / / / / / /  // / / /  /
+
+export const NavWrapper = styled.nav`
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 3em 0;
+    z-index: 9999;
+    flex-direction: row;
+    width: 100%;
+    background-color: ${colors.offWhiteBackground};
+`
+export const NavItems = styled.ul<{menuIsOpen: boolean}>`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    @media(max-width: ${containerWidth.tablet}) {
+        position: fixed;
+        left: ${(props) => (props.menuIsOpen ? '50%' : '100%')};
+        top: 8.23rem;
+        flex-direction: column;
+        background-color: ${colors.offWhiteBackground};
+        width: 50%;
+        height: 100%;
+        text-align: center;
+        transition: 0.3s all ease;
+        justify-content: initial; 
+    }
+`
+
+export const NavItem = styled.li`
+    list-style-type: none;
+
+    @media(max-width: ${containerWidth.tablet}) {
+        margin: 1.5rem 0;
+    }
+`
+
+export const Hamburger = styled.button<{menuIsOpen: boolean}>`
+    display: none;
+    border: none;
+    background: transparent;
+
+
+    & span {
+        display: block;
+        width: 25px;
+        height: 3px;
+        margin: 5px auto;
+        -webkit-transition: all 0.3s ease-in-out;
+        transition: all 0.3s ease-in-out;
+        background-color: ${colors.secondary};
+
+        &:first-child {
+            transform: ${(props) => (props.menuIsOpen ? 'translateY(8px) rotate(45deg);' : 'none')};
+        }
+
+        &:nth-child(2) {
+            opacity: ${(props) => (props.menuIsOpen ? '0' : '1')};
+        }
+
+        &:last-child {
+            transform: ${(props) => (props.menuIsOpen ? 'translateY(-8px) rotate(-45deg);' : 'none')};
+        }
+    }
+
+    @media(max-width: ${containerWidth.tablet}) {
+        display: block;
+        cursor: pointer;
+    }
+`
 
--- src/containers/Navbar/Navbar.tsx
+++ src/containers/Navbar/Navbar.tsx
@@ -1,32 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Nav,
   NavbarLogo,
   NavbarContainer,
-  NavbarItems
+  NavbarItems,
+  NavWrapper,
+  NavItems,
+  NavItem,
+  Hamburger
 } from './Navbar.components'
 import { NavbarProps } from './Navbar.types'
 import { CustomLinkUnderline } from '../../components/Transitions/LinkCustom.components'
-import { Row, Col } from '../../lib/Grid'
+import Container from '../../containers/Container';
+
 
 
 const Navbar: React.FC<NavbarProps> = () => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+  const toggleMenu = () => setMenuIsOpen(!menuIsOpen);
+
   return (
-    <Nav id="nav">
-      <NavbarContainer>
-        <Row justify="between">
-          <Col>
-            <NavbarLogo>Lars van der Niet</NavbarLogo>
-          </Col>
-          <Col xs="content">
-            <NavbarItems>
-              <CustomLinkUnderline url={'/'} effect="fade" lenght={.3}>Werk</CustomLinkUnderline>
-              <CustomLinkUnderline url={'/about'} effect="fade" lenght={.3}>Over Lars</CustomLinkUnderline>
-            </NavbarItems>
-          </Col>
-        </Row>
-      </NavbarContainer>
-    </Nav>
+    <NavWrapper id="nav">
+      <Container>
+        <NavbarLogo>Lars van der Niet</NavbarLogo>
+      </Container>
+      <Container>
+        <NavItems menuIsOpen={menuIsOpen}>
+          <NavItem onClick={toggleMenu}>
+            <CustomLinkUnderline url={'/'} effect="fade" lenght={.3}>Werk</CustomLinkUnderline>
+          </NavItem>
+          <NavItem onClick={toggleMenu}>
+            <CustomLinkUnderline url={'/about'} effect="fade" lenght={.3}>Over Lars</CustomLinkUnderline>
+          </NavItem>
+        </NavItems>
+        <Hamburger menuIsOpen={menuIsOpen} onClick={toggleMenu}>
+          <span />
+          <span />
+          <span />
+        </Hamburger>
+      </Container>
+    </NavWrapper>
   );
 };
 
