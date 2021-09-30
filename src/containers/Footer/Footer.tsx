15d8d73a6975b3f5960c9481d5fc1301f5da815e
Lars van der Niet <larsvanderniet@gmail.com>
2021-09-30 11:21:39 +0200

    Updated credits

--- src/containers/Footer/Footer.tsx
+++ src/containers/Footer/Footer.tsx
@@ -23,14 +23,14 @@ const Footer: React.FC<FooterProps> = () => {
   return (
     <FooterContainer>
       <WaveContainer>
-        <Wave 
+        <Wave
           fill={colors.secondary}
           options={{
             height: 20,
             amplitude: 70,
             speed: 0.1,
             points: 3
-          }}/>
+          }} />
       </WaveContainer>
       <FooterContent>
         <Container>
@@ -42,11 +42,11 @@ const Footer: React.FC<FooterProps> = () => {
               <ContactMe>Om meer te weten te komen, of voor eventuele samenwerkingen, <a href="mailto:lvdnbusiness@icloud.com" target="_blank">laat een mailtje achter</a> of vindt me op social media.</ContactMe>
               <IconContext.Provider value={{ color: "white", size: "2em" }}>
                 <SocialsContainer>
-                      <a href="https://www.instagram.com/lvdniet/" target="_blank" rel="noopener" aria-label="Instagram"><FaInstagram /></a>
-                      <a href="https://twitter.com/larsvdniet" target="_blank" rel="noopener" aria-label="Twitter"><FaTwitter /></a>
+                  <a href="https://www.instagram.com/lvdniet/" target="_blank" rel="noopener" aria-label="Instagram"><FaInstagram /></a>
+                  <a href="https://twitter.com/larsvdniet" target="_blank" rel="noopener" aria-label="Twitter"><FaTwitter /></a>
                 </SocialsContainer>
               </IconContext.Provider>
-              <BuildBy>Drawings by <a href="https://www.instagram.com/mattoe_zandee/" target="_blank" rel="noopener">Tejo Zandee</a></BuildBy>
+              <BuildBy>Animaties door <a href="https://www.instagram.com/mattoe_zandee/" target="_blank" rel="noopener">Tejo Zandee</a></BuildBy>
             </Col>
           </Row>
         </Container>
