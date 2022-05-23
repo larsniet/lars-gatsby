4af12b9d30319a2f66cb0cd9dd659eebd80113fd
Lars van der Niet <lvdnbusiness@icloud.com>
2022-05-23 18:25:41 +0200

    Updated analytics tag

--- src/lib/getHeadScripts.tsx
+++ src/lib/getHeadScripts.tsx
@@ -1,31 +1,16 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
-const enableTagManager = true;
-const tagManagerCode = `
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-5BMZBGJ');
+const analyticsSiteTag = `
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-3TK3C2FLSK"></script>
+  <script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-3TK3C2FLSK');
+  </script>
 `;
-
-const NoscriptTag = () => {
-  if (enableTagManager) {
-    return (
-      <noscript>
-        <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-5BMZBGJ"
-          height="0"
-          width="0"
-          title="tagmanager"
-          style={{ display: 'none', visibility: 'hidden' }}
-        />
-      </noscript>
-    );
-  }
-  return null;
-};
 
 const HeadScripts: React.FC<Record<string, never>> = () => {
   return (
@@ -34,9 +19,8 @@ const HeadScripts: React.FC<Record<string, never>> = () => {
         <meta name="robots" content="index, follow" />
         <link type="text/plain" rel="author" href="/humans.txt" />
         <script type="text/javascript">
-          ${enableTagManager && tagManagerCode}
+          ${analyticsSiteTag}
         </script>
-        ${<NoscriptTag />}
       `}
     </Helmet >
   )
